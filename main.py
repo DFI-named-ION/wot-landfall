20f9f0b0836d6aeb83a7a5475c47f361b88e5c6a
DFI-named-ION <larityandselnter@gmail.com>
2024-12-01 00:08:55 -0500

    testing messages

---
+++ main.py
@@ -0,0 +1,8 @@
+﻿import BigWorld
+from gui import SystemMessages
+
+def _loop():
+    SystemMessages.pushMessage('Hello!1!', type=SystemMessages.SM_TYPE.Warning)
+    BigWorld.callback(2, _loop)
+
+_loop()
